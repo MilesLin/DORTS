c3d487b98a1595642ac9fe3db28ebe8285830421
Miles <milesasp@gmail.com>
2019-11-26 17:35:24 +0800

    P8

---
+++ Day8/LINQ_Practice/LINQ_Practice/P8.cs
@@ -0,0 +1,38 @@
+﻿using ExpectedObjects;
+using LINQ_Practice.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Practice
+{
+    [TestClass]
+    public class P8
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /*
+                題目: 請將兩個陣列的值組合在一起
+            */
+
+            // Arrange
+            int[] numbers = { 3, 5, 7 };
+            string[] words = { "three", "five", "seven" };
+
+
+            // 請更改這段程式碼，並完成題目
+            IEnumerable<string> result = numbers.Zip(words, (n, w) => n + "=" + w);
+
+            // Assert
+            var expected = new List<string>
+            {
+                "3=three",
+                "5=five",
+                "7=seven"
+            };
+
+            expected.ToExpectedObject().ShouldMatch(result);
+        }
+    }    
+}
