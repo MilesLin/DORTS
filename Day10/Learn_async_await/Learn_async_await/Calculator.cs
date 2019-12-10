b1a2b8d1729b2b8200a6963f23591a6e720dc292
Miles <milesasp@gmail.com>
2019-12-10 14:35:41 +0800

    Learn_async_await

---
+++ Day10/Learn_async_await/Learn_async_await/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Learn_async_await
+{
+    internal class Calculator
+    {
+        public async Task<int> GetSumAsync(int a, int b)
+        {
+            Console.WriteLine("步驟 2 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+
+            int result = await Task.Run(() =>
+            {
+                Console.WriteLine("步驟 3 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+                return a + b;
+            });
+
+            Console.WriteLine("步驟 4 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            return result;
+        }
+    }
+}
---
+++ Day10/Learn_async_await/Learn_async_await/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Learn_async_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+
+            Console.WriteLine("步驟 1 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            Task<int> sum = cal.GetSumAsync(1, 10);
+            Console.WriteLine("步驟 5 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("結果:" + sum.Result);
+            Console.WriteLine("步驟 6 => ThreadID : " + Thread.CurrentThread.ManagedThreadId);
+            Console.Read();
+
+        }
+    }
+}
