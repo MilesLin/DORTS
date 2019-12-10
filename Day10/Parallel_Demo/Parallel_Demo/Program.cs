d715a4714b2486c06dba363ef0f4faa2e922e4ae
Miles <milesasp@gmail.com>
2019-12-10 11:37:10 +0800

    Parallel_Demo

---
+++ Day10/Parallel_Demo/Parallel_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../TestFiles");
+
+            var watch = new Stopwatch();
+
+            watch.Start();
+
+            Parallel.ForEach(files, (currentFile) =>
+            {
+                string filename = Path.GetFileName(currentFile);
+                Console.WriteLine($"Processing {filename} on thread {Thread.CurrentThread.ManagedThreadId}");
+            });
+            
+            watch.Stop();
+
+            Console.WriteLine("Consume Time: {0} ms", watch.ElapsedMilliseconds);
+        }
+    }
+}
