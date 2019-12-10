b54d8fe40a9658c272948b62c1e70caf758708b4
Miles <milesasp@gmail.com>
2019-12-10 14:58:46 +0800

    Block UI and CRUD_async_await

---
+++ Day10/CRUD_async_await/after/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity2.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Telephone { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
