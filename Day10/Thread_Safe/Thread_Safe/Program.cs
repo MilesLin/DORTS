84b95b0cd79fa8092f96e1c70f4d679bc46db84a
Miles <milesasp@gmail.com>
2019-12-10 15:22:06 +0800

    Thread_Safe

---
+++ Day10/Thread_Safe/Thread_Safe/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Safe
+{
+    class Program
+    {
+        private static object lockObj = new object();
+        
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            var t1 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    lock (lockObj)
+                    {
+                        counter++;
+                    }
+                }
+            });
+
+            var t2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    lock (lockObj)
+                    {
+                        counter++;
+                    }
+                }
+            });
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine($"結果是: {counter}");
+        }
+    }
+}
---
+++ Day10/Thread_Safe_List/Thread_Safe_List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thread_Safe_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<string> r = new ConcurrentBag<string>();
+            
+            var t1 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    r.Add(i.ToString());
+                }
+            });
+
+            var t2 = Task.Run(() =>
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    r.Add(i.ToString());
+                }
+            });
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine($"共有 {r.Count} 筆資料");
+
+        }
+    }
+}
